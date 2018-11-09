091fd3abc26a60f0a84e89aa685343e6ef57ae2c
Nando Pena <fernando.pena@rackspace.com>
2018-11-09 02:52:08 -0600

    Ultimate Bingo Deathmatch - add dice

---
+++ ultimate-bingo-deathmatch/squib/dice.rb
@@ -0,0 +1,70 @@
+require 'squib'
+
+Squib::Deck.new(cards: 60, width: 188, height: 188) do
+  # background color: '#ffffff'
+
+  # png file: './assets/dicesticker.png'
+  text range: 0, str: '1', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 1, str: '2', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 2, str: '3', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 3, str: '4', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 4, str: '5', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 5, str: 'B', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 6, str: 'I', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 7, str: 'N', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 8, str: 'G', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  text range: 9, str: 'O', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#236CDD"
+  png range: 10, file: './assets/blue_energy.png', x: 73, y: 52, width: 40, height: 80
+  png range: 11, file: './assets/blue_energy.png', x: 73, y: 52, width: 40, height: 80
+  text range: 12, str: '1', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 13, str: '2', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 14, str: '3', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 15, str: '4', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 16, str: '5', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 17, str: 'B', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 18, str: 'I', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 19, str: 'N', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 20, str: 'G', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  text range: 21, str: 'O', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#DA3941"
+  png range: 22, file: './assets/red_energy.png', x: 73, y: 52, width: 40, height: 80
+  png range: 23, file: './assets/red_energy.png', x: 73, y: 52, width: 40, height: 80
+  text range: 24, str: '1', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 25, str: '2', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 26, str: '3', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 27, str: '4', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 28, str: '5', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 29, str: 'B', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 30, str: 'I', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 31, str: 'N', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 32, str: 'G', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  text range: 33, str: 'O', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#3DBB34"
+  png range: 34, file: './assets/green_energy.png', x: 73, y: 52, width: 40, height: 80
+  png range: 35, file: './assets/green_energy.png', x: 73, y: 52, width: 40, height: 80
+  text range: 36, str: '1', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 37, str: '2', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 38, str: '3', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 39, str: '4', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 40, str: '5', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 41, str: 'B', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 42, str: 'I', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 43, str: 'N', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 44, str: 'G', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  text range: 45, str: 'O', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#643E99"
+  png range: 46, file: './assets/purple_energy.png', x: 73, y: 52, width: 40, height: 80
+  png range: 47, file: './assets/purple_energy.png', x: 73, y: 52, width: 40, height: 80
+  text range: 48, str: '1', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 49, str: '2', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 50, str: '3', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 51, str: '4', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 52, str: '5', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 53, str: 'B', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 54, str: 'I', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 55, str: 'N', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 56, str: 'G', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  text range: 57, str: 'O', x: 0, y: 40, width: 188, height: 50, align: :center, font: 'Arial 24', color: "#D4B54E"
+  png range: 58, file: './assets/yellow_energy.png', x: 73, y: 52, width: 40, height: 80
+  png range: 59, file: './assets/yellow_energy.png', x: 73, y: 52, width: 40, height: 80
+
+  save format: :png, prefix: 'dice_'
+  # save_pdf file: 'lazors_sheet.pdf'
+end
