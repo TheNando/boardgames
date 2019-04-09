ed2b44ea99bd367a69c36533a6c156e4f4f180db
Nando Pena <fernando.pena@rackspace.com>
2019-04-09 03:11:46 -0500

    UBD: Update batteries, lazors, WIP pdfs

--- ultimate-bingo-deathmatch/squib/batteries.rb
+++ ultimate-bingo-deathmatch/squib/batteries.rb
@@ -10,9 +10,6 @@ deck = Squib.csv file: '../data/batteries.csv'
   # Card Type Icon
   png file: './assets/battery.png', layout: :BatteryIcon
 
-  # Energy Cost Icon
-  png range: 5..14, file: './assets/energy.png', layout: :EnergyIcon
-
   # Generate main card images array
   battery_images = deck['Name'].map do |f|
     "./assets/battery/#{f.to_s.tr_s(' ', '-')}.png"
@@ -25,20 +22,47 @@ deck = Squib.csv file: '../data/batteries.csv'
   text str: deck['Name'], layout: :Title2
   text str: deck['Name'], layout: :Title
 
-  # Card Cost
-  text str: deck['Cost'], layout: :EnergyCost
-
-  # Battery Capacity
-  text str: 'Capacity:', layout: :CapacityLabel
-  text str: deck['Capacity'], layout: :Capacity
+  # Tier
+  text str: deck['Tier'], layout: :Tier2
+  text range: 0..4, str: deck['Tier'], layout: :Tier, color: '#9dcb51'
+  text range: 5..10, str: deck['Tier'], layout: :Tier, color: '#24bfc4'
+  text range: 11..14, str: deck['Tier'], layout: :Tier, color: '#c593c5'
 
   # Battery Passive effect
-  text str: 'At the start of the round:', layout: :PassiveLabel
-  text str: deck['Passive'], layout: :Passive
+  text range: 5..12, str: 'Passive', layout: :PassiveLabel
+  text(range: 5..12, str: deck['Passive'], layout: :PassiveDescription) do |embed|
+    embed.png key: ':energy:', file: './assets/energy.png', layout: :EnergyIcon
+  end
 
-  # Battery Active effect
-  text str: 'Activate:', layout: :ActivateLabel
-  text str: deck['Activate'], layout: :Activate
+  text range: 13..14, str: 'Passive', layout: :PassiveLabelRaised
+  text(range: 13..14, str: deck['Passive'], layout: :PassiveDescriptionRaised) do |embed|
+    embed.png key: ':energy:', file: './assets/energy.png', layout: :EnergyIcon
+  end
+
+  # Activation Text
+  text range: 0..4, str: 'Activate', layout: :ActivateLabelBasic
+  text range: 0..4, str: deck['Activate'], markup: true, layout: :ActivateDescriptionBasic
+
+  # Activation Text
+  text range: 5..12, str: 'Activate', layout: :ActivateLabel
+  text(range: 5..12, str: deck['Activate'], layout: :ActivateDescription) do |embed|
+    embed.png key: ':energy:', file: './assets/energy.png', layout: :EnergyIcon
+  end
+
+  text range: 13..14, str: 'Activate', layout: :ActivateLabelRaised
+  text(range: 13..14, str: deck['Activate'], layout: :ActivateDescriptionRaised) do |embed|
+    embed.png key: ':energy:', file: './assets/energy.png', layout: :EnergyIcon
+  end
+
+  # Overcharge Bank
+  ocs = deck['Capacity']
+  0.upto(5).each do |n|
+    range = ocs.each_index.select { |i| ocs[i] == n}
+    n.times do |i|
+      png file: './assets/oc-space.png', layout: :OCSpace, range: range,
+          alpha: 0.75, x: 600 - (i * 140)
+    end
+  end
 
   # save format: :png, prefix: 'batteries_'
   save_pdf file: 'batteries_sheet.pdf'
